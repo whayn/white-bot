cc570853b06b4c97596f874a2ce1044c4e57de07
Yamegto <mayeul.falgayrettes@gmail.com>
2021-02-24 18:57:13 +0100

    Modify gititgnore and rank command

--- events/message/commands/rank.js
+++ events/message/commands/rank.js
@@ -2,14 +2,15 @@ const { existsSync } = require('fs');
 
 module.exports = {
     exec: async ({ message, args }) => {
-        const member = message.guild.members.cache.get(args[0] && args[0].replace(/\D+/g, '')) || message.member;
-        const user_exp = existsSync(`./database/users/${message.author.id}.json`) ? require(`../../../_database/users/exp/${message.author.id}.json`) : { level: 0, exp: 0 };
+        const member = message.guild.members.cache.get(args[0] && args[0].replace(/\D+/g, "")) || message.member;
+        const user_exp = existsSync(`./_database/users/exp/${message.author.id}.json`) ? require(`../../../_database/users/exp/${message.author.id}.json`) : { level: 0, exp: 0 };
 
         message.channel.send({
             embed: {
+                color: "53935",
                 author: { name: `Niveau de ${member.user.tag}`, icon_url: member.user.displayAvatarURL({ size: 2048, dynamic: true, format: "png" }) },
                 fields: [
-                    { title: "Niveau", value: user_exp.level, inline: true },
+                    { name: "Niveau", value: user_exp.level, inline: true },
                     { name: "Experience", value: user_exp.exp, inline: true }
                 ]
             }
